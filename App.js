5d8d879bf410ee1bce3bd6ad035595273732f689
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-06-01 11:22:10 +0530

    init

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {View} from 'react-native';
+import StackNavExample from './src/navigationComponent/StackNavExample';
+
+const App = () => {
+  return (
+    <View>
+      <StackNavExample />
+    </View>
+  );
+};
+
+export default App;
---
+++ src/navigationComponent/StackNavExample.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const StackNavExample = () => {
+  return (
+    <View>
+      <Text>StackNavExample</Text>
+    </View>
+  );
+};
+
+export default StackNavExample;
