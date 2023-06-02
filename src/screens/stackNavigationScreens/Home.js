257692f302fdcf0da2689f6b5cb0e0c0448e6352
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-06-02 13:29:36 +0530

    feat : bottom navigator screesns

--- src/screens/stackNavigationScreens/Home.js
+++ src/screens/stackNavigationScreens/Home.js
@@ -1,23 +1,33 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import homeStyle from '../../style/homeComponentStyle';
+import 'react-native-gesture-handler';
+import DrawerNavigation from '../../navigationComponent/DrawerNavigation';
 
-const Home = props => {
-  console.log(props.route.params);
-  const {email, pass} = props.route.params;
+const Home = () => {
   return (
-    <View style={homeStyle.view}>
-      <Text style={homeStyle.text}>Home Screen</Text>
-      <View style={[homeStyle.getText, homeStyle.getTextView]}>
-        <Text style={[homeStyle.getText, homeStyle.getTextEmail]}>
-          Email : {email}
-        </Text>
-        <Text style={[homeStyle.getText, homeStyle.getTextPass]}>
-          Password : {pass}
-        </Text>
-      </View>
+    <View style={{flex: 1}}>
+      <DrawerNavigation />
     </View>
   );
 };
+
+// const Home = props => {
+//   console.log(props.route.params);
+//   const {email, pass} = props.route.params;
+//   return (
+//     <View style={homeStyle.view}>
+//       <Text style={homeStyle.text}>Home Screen</Text>
+//       <View style={[homeStyle.getText, homeStyle.getTextView]}>
+//         <Text style={[homeStyle.getText, homeStyle.getTextEmail]}>
+//           Email : {email}
+//         </Text>
+//         <Text style={[homeStyle.getText, homeStyle.getTextPass]}>
+//           Password : {pass}
+//         </Text>
+//       </View>
+//     </View>
+//   );
+// };
 
 export default Home;
