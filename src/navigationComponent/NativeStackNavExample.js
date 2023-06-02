6bc9b99f2d925cca17d0c3a821361017c471b34e
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-06-02 13:29:11 +0530

    feat : stack navigator

--- src/navigationComponent/NativeStackNavExample.js
+++ src/navigationComponent/NativeStackNavExample.js
@@ -21,29 +21,30 @@ const StackNavExample = () => {
           },
           headerTitleStyle: {fontSize: 20},
         }}>
-        <Stack.Screen name="Login" component={Login} />
+        {/* <Stack.Screen name="Login" component={Login} /> */}
         <Stack.Screen
           name="Home"
           component={Home}
-          options={{
-            headerRight: () => (
-              <View>
-                <TouchableOpacity
-                  activeOpacity={0.7}
-                  onPress={btnPressed}
-                  style={{
-                    backgroundColor: 'darkgreen',
-                    padding: 5,
-                    borderRadius: 5,
-                  }}>
-                  <Text style={{fontWeight: 500, color: '#fff'}}>Quit</Text>
-                </TouchableOpacity>
-              </View>
-            ),
-            headerStyle: {
-              backgroundColor: 'lightgreen',
-            },
-          }}
+          options={{headerShown: false}}
+          // options={{
+          //   headerRight: () => (
+          //     <View>
+          //       <TouchableOpacity
+          //         activeOpacity={0.7}
+          //         onPress={btnPressed}
+          //         style={{
+          //           backgroundColor: 'darkgreen',
+          //           padding: 5,
+          //           borderRadius: 5,
+          //         }}>
+          //         <Text style={{fontWeight: 500, color: '#fff'}}>Quit</Text>
+          //       </TouchableOpacity>
+          //     </View>
+          //   ),
+          //   headerStyle: {
+          //     backgroundColor: 'lightgreen',
+          //   },
+          // }}
         />
       </Stack.Navigator>
     </NavigationContainer>
