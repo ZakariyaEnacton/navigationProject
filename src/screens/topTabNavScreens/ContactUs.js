9bc3add30134b427b10a2aa765cbb87f5ef04108
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-06-05 18:48:36 +0530

    feat : contact Sreen for tab navigation

---
+++ src/screens/topTabNavScreens/ContactUs.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import contactStyle from '../../style/contactComponentStyle';
+
+const ContactUs = () => {
+  return (
+    <View style={contactStyle.view}>
+      <Text style={contactStyle.text}>Contact Us</Text>
+    </View>
+  );
+};
+
+export default ContactUs;
