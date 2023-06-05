26f8d041979776d0e0d471a3c6a7c5161adee36b
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-06-05 18:46:58 +0530

    feat : bottom navigator

--- src/navigationComponent/BottomNavigator.js
+++ src/navigationComponent/BottomNavigator.js
@@ -1,30 +1,43 @@
 import React from 'react';
-import Screen1 from '../screens/bootomNavScreens/Screen1';
 import Screen2 from '../screens/bootomNavScreens/Screen2';
 import Screen3 from '../screens/bootomNavScreens/Screen3';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import DrawerNavigation from './DrawerNavigation';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/AntDesign';
+import TabNavigator from './TabNavigator';
 
-const BottomNavigator = () => {
+const BottomNavigator = props => {
   const Bottom = createMaterialBottomTabNavigator();
   return (
     <Bottom.Navigator>
       <Bottom.Screen
         name="Screen1"
-        component={Screen1}
+        component={DrawerNavigation}
+        initialParams={props.route.params}
         options={{
+          tabBarIcon: () => <Icon name="home" size={24} />,
           headerShown: false,
           tabBarLabel: 'Home',
         }}
       />
       <Bottom.Screen
         name="Screen2"
-        component={Screen2}
-        options={{headerShown: false, tabBarLabel: 'Setting'}}
+        component={TabNavigator}
+        options={{
+          headerShown: false,
+          tabBarLabel: 'Setting',
+          tabBarIcon: () => <Icon name="setting" size={24} />,
+        }}
       />
       <Bottom.Screen
         name="Screen3"
         component={Screen3}
-        options={{headerShown: false, tabBarLabel: 'Profile'}}
+        options={{
+          tabBarIcon: () => <Icon name="profile" size={24} />,
+          headerShown: false,
+          tabBarLabel: 'Profile',
+        }}
       />
     </Bottom.Navigator>
   );
