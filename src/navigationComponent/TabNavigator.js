82f6757143331dc852f22d161001952ec551f7b7
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-06-05 18:47:18 +0530

    feat : tab navigation

--- src/navigationComponent/TabNavigator.js
+++ src/navigationComponent/TabNavigator.js
@@ -4,17 +4,19 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import Home from '../screens/stackNavigationScreens/Home';
 import Login from '../screens/stackNavigationScreens/Login';
+import Screen2 from '../screens/bootomNavScreens/Screen2';
+import ContactUs from '../screens/topTabNavScreens/ContactUs';
+import Help from '../screens/topTabNavScreens/Help';
 
 const Top = createMaterialTopTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <NavigationContainer>
-      <Top.Navigator>
-        <Top.Screen name="Home" component={Home} />
-        <Top.Screen name="Login" component={Login} />
-      </Top.Navigator>
-    </NavigationContainer>
+    <Top.Navigator>
+      <Top.Screen name="Settings" component={Screen2} />
+      <Top.Screen name="Contact Us" component={ContactUs} />
+      <Top.Screen name="Help" component={Help} />
+    </Top.Navigator>
   );
 };
 
