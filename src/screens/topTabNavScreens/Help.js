5cce3bcb3ca870d1fe49a80d2ab7cfa9ddc6359b
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-06-05 18:48:49 +0530

    feat : helpScreen for tab navigation

---
+++ src/screens/topTabNavScreens/Help.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import helpStyle from '../../style/helpComponentStyle';
+
+const Help = () => {
+  return (
+    <View style={helpStyle.view}>
+      <Text style={helpStyle.text}>Help</Text>
+    </View>
+  );
+};
+
+export default Help;
