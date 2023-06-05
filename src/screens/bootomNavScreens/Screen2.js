7a468778662538325a5b682c60417d684183793a
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-06-05 18:47:44 +0530

    feat : bottom nav screen 2

--- src/screens/bootomNavScreens/Screen2.js
+++ src/screens/bootomNavScreens/Screen2.js
@@ -1,10 +1,11 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import screenStyle from '../../style/ScreenStyle';
 
 const Screen2 = () => {
   return (
-    <View>
-      <Text>Screen2</Text>
+    <View style={screenStyle.view}>
+      <Text style={screenStyle.text}>Setting</Text>
     </View>
   );
 };
