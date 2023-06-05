64d8656f52018e118f7753d207c8013a3147bdcc
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-06-05 18:48:18 +0530

    feat : homeScreen for stack navigation

--- src/screens/stackNavigationScreens/Home.js
+++ src/screens/stackNavigationScreens/Home.js
@@ -1,13 +1,33 @@
-import {View, Text} from 'react-native';
+import {View, Text, Touchable, TouchableOpacity} from 'react-native';
 import React from 'react';
 import homeStyle from '../../style/homeComponentStyle';
 import 'react-native-gesture-handler';
-import DrawerNavigation from '../../navigationComponent/DrawerNavigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const Home = () => {
+const Home = (props, {route, navigation}) => {
+  const {email, pass} = props.route.params;
+  console.log(route);
+  console.log(navigation);
   return (
     <View style={{flex: 1}}>
-      <DrawerNavigation />
+      <View style={homeStyle.view}>
+        <Text style={homeStyle.text}>Home Screen</Text>
+        <View style={[homeStyle.getText, homeStyle.getTextView]}>
+          <Text style={[homeStyle.getText, homeStyle.getTextEmail]}>
+            Email : {email}
+          </Text>
+          <Text style={[homeStyle.getText, homeStyle.getTextPass]}>
+            Password : {pass}
+          </Text>
+        </View>
+        <TouchableOpacity
+          onPress={() => props.navigation.openDrawer}
+          activeOpacity={0.7}
+          style={homeStyle.button}>
+          {console.log('nav-----------', props.navigation.openDrawer)}
+          <Text>Open Drawer</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
